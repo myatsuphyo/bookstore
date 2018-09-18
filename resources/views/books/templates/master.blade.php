877a67bbb68aaa05990fc5b95b4a192272ba1de0
myatsuphyo <myatsuphyo9@gmail.com>
2018-09-18 15:32:12 +0630

    second commit

---
+++ resources/views/books/templates/master.blade.php
@@ -0,0 +1,14 @@
+@include('books.templates.partials.header')
+
+<!-- container -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+
+            @yield('content')
+
+        </div>
+    </div>
+</div><!-- container -->
+
+@include('books.templates.partials.footer')
